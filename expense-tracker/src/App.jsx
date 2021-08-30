ad30103595048b0191aab0f196b90919f756b0b8
Sam Hollyer <rsamhollyer@gmail.com>
2021-08-30 11:55:47 -0500

    initial expense tracker

---
+++ expense-tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" />
+    </div>
+  );
+}
+
+export default App;
