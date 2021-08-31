0cb413c24decad93d5a602adeaf4b2e037a92241
Sam Hollyer <rsamhollyer@gmail.com>
2021-08-31 10:31:44 -0500

    add use state

--- expense-tracker/src/components/ExpenseItem/ExpenseItem.jsx
+++ expense-tracker/src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../Card/Card';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import './ExpenseItem.css';
 
 export default function ExpenseItem({ title, amount, date }) {
+  const [stateTitle, setStateTitle] = useState(title);
+
   const handleClick = () => {
-    console.log('Clicked');
+    setStateTitle('Updated');
   };
 
   return (
     <Card className="expense-item">
       {date && <ExpenseDate date={date} />}
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{stateTitle}</h2>
       </div>
       <div className="expense-item__price">${amount}</div>
       <button onClick={handleClick} type="button">
