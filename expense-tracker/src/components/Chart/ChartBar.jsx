5ee9b0bf06a90866021587f04cd7274e56ed2578
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-01 14:53:55 -0500

    chartbar comp fleshed out

--- expense-tracker/src/components/Chart/ChartBar.jsx
+++ expense-tracker/src/components/Chart/ChartBar.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './styles/Chartbar.css';
 
-export default function ChartBar() {
-  return <div />;
+export default function ChartBar({ value, maxValue, label }) {
+  const valueRatio = () => {
+    const fillHeight = `${Math.round((value / maxValue) * 100)}%` || '0%';
+    return fillHeight;
+  };
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill" style={{ height: valueRatio() }} />
+      </div>
+      <div className="chart-bar__label">{label}</div>
+    </div>
+  );
 }
