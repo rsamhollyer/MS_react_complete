63a66310016051f8e5f72b9d162b5e0beeed15ec
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-02 14:44:20 -0500

    initial push

---
+++ add_users/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AddUser from './components/AddUser';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AddUser />
+      </header>
+    </div>
+  );
+}
+
+export default App;
