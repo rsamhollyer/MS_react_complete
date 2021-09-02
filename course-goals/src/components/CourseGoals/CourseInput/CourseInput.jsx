e0d9fbb79becc5976e7c25b1fa86d0b59f510280
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-02 09:52:24 -0500

    implement css modules in inputs

--- course-goals/src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ course-goals/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
+import classes from './CourseInput.module.css';
 
 const CourseInput = ({ onAddGoal }) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -25,10 +26,12 @@ const CourseInput = ({ onAddGoal }) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl valid={isValid}>
+      <div
+        className={`${classes['form-control']} ${!isValid && classes.invalid}`}
+      >
         <label htmlFor="course-goal">Course Goal</label>
         <input id="course-goal" type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
@@ -36,29 +39,29 @@ const CourseInput = ({ onAddGoal }) => {
 
 export default CourseInput;
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${({ valid }) => (valid ? 'black' : 'red')};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${({ valid }) => (valid ? 'black' : 'red')};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${({ valid }) => (valid ? '#ccc' : 'red')};
-    background: ${({ valid }) => (valid ? 'transparent' : 'salmon')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${({ valid }) => (valid ? '#ccc' : 'red')};
+//     background: ${({ valid }) => (valid ? 'transparent' : 'salmon')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
