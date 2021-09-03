f51ca43b2046f63a8d8d4b47ae7ba7a3f3ae627f
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-03 16:06:24 -0500

    add cleanup debounce fnc

--- side_effects/src/components/Login/Login.jsx
+++ side_effects/src/components/Login/Login.jsx
@@ -12,10 +12,15 @@ export default function Login({ onLogin }) {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-    return () => {};
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 750);
+
+    return () => {
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = event => {
