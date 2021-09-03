2648c46e842b139cf9c57b120f91a6be6d561abb
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-03 03:09:20 -0500

    fixed user comp

--- add_users/src/components/AddUser.jsx
+++ add_users/src/components/AddUser.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
 export default function AddUser() {
-  return <div>USER</div>;
+  const submitHandler = e => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">UserName</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </div>
+  );
 }
