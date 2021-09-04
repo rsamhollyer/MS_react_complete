03199184d63e5979ca639bccb2903d2757fdbcf0
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-03 22:05:08 -0500

    header card button add

--- food-order/src/components/Header/Header.jsx
+++ food-order/src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 export default function Header() {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button type="button">Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="meals background" />
---
+++ food-order/src/components/Header/HeaderCartButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
+
+export default function HeaderCartButton() {
+  return (
+    <button className={classes.button} type="button">
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>4</span>
+    </button>
+  );
+}
