3b63f1b667e395c7e903abd374c4369364857e11
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-04 22:43:41 -0500

    finished project

--- food-order/src/components/Header/HeaderCartButton.jsx
+++ food-order/src/components/Header/HeaderCartButton.jsx
@@ -1,17 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 export default function HeaderCartButton({ onClick }) {
   const cartCtx = useContext(CartContext);
-
+  const [isAnimated, setIsAnimated] = useState(false);
   const numberOfCartItems = cartCtx.items.reduce(
     (totalItems, currentItem) => totalItems + currentItem.amount,
     0
   );
+
+  const btnClasses = `${classes.button} ${isAnimated ? classes.bump : ''}`;
+  useEffect(() => {
+    if (cartCtx.items.length === 0) return;
+    setIsAnimated(true);
+
+    const timer = setTimeout(() => {
+      setIsAnimated(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
   return (
-    <button className={classes.button} type="button" onClick={onClick}>
+    <button className={btnClasses} type="button" onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
