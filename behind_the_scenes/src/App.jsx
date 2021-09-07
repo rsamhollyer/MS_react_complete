71722101631f426695bf43aae198bd93c91afa1f
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-07 17:24:05 -0500

    small change

--- behind_the_scenes/src/App.jsx
+++ behind_the_scenes/src/App.jsx
@@ -5,16 +5,22 @@ import DemoOutput from './components/DemoOutput';
 
 function App() {
   const [showPara, setShowPara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
   console.log('APP RUNNING');
 
   const togglePara = useCallback(() => {
-    setShowPara((prev) => !prev);
-  }, [setShowPara]);
+    if (allowToggle) setShowPara((prev) => !prev);
+  }, [allowToggle]);
+
+  const toggleToggle = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showPara} />
+      <Button onClick={toggleToggle}>Allow ParaGraph</Button>
       <Button onClick={togglePara}>Toggle ParaGraph</Button>
     </div>
   );
