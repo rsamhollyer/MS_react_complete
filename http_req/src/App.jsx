41124601ab9367f1a03a002519aab4b3c99495fe
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-09 14:29:09 -0500

    add fetch movies

--- http_req/src/App.jsx
+++ http_req/src/App.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const fetchMoviesHandler = () => {
+    const url = `https://swapi.dev/api/films`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const transformedMovies = data.results.map(movie => ({
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        }));
+        setMovies(transformedMovies);
+      })
+      .catch();
+  };
 
   return (
     <>
       <section>
-        <button type="button">Fetch Movies</button>
+        <button type="button" onClick={fetchMoviesHandler}>
+          Fetch Movies
+        </button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </>
   );
