c13afd27a4dfc5f7bf57b2b17922fce8dc2207b3
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-10 16:30:10 -0500

    add use hook fnc to app

--- more_hooks/src/App.jsx
+++ more_hooks/src/App.jsx
@@ -1,17 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 import useHTTP from './hooks/useHTTP';
 
 function App() {
-  const requestConfig = {
-    url: 'https://react-http-af080-default-rtdb.firebaseio.com/tasks.json',
-  };
-
   const [tasks, setTasks] = useState([]);
 
-  const transformedTasks = tasksObject => {
+  const transformedTasks = useCallback(tasksObject => {
     const loadedTasks = [];
 
     Object.keys(tasksObject).forEach(key =>
@@ -19,17 +15,20 @@ function App() {
     );
 
     setTasks(loadedTasks);
-  };
+  }, []);
 
   const {
     isLoading,
     error,
     sendRequest: fetchTasks,
-  } = useHTTP(requestConfig, transformedTasks);
+  } = useHTTP(transformedTasks);
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({
+      url: 'https://react-http-af080-default-rtdb.firebaseio.com/tasks.json',
+    });
+    return () => {};
+  }, [fetchTasks]);
 
   const taskAddHandler = task => {
     setTasks(prevTasks => prevTasks.concat(task));
--- more_hooks/src/hooks/useHTTP.jsx
+++ more_hooks/src/hooks/useHTTP.jsx
@@ -1,43 +1,33 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
-/**
- * @namespace useHTTP
- *
- * @param {object} requestConfig - object containing some fetch() configurable information
- * @param {string} requestConfig.url  Url string
- * @param {string} requestConfig.method  Requested HTTP method
- * @param {string} requestConfig.headers  Request Headers to send
- * @param {string=} requestConfig.body  Request body sent with required methods
- *
- * @param {function} applyData  - setter function that should update the state where data loads
- *
- */
-
-export default function useHTTP(requestConfig, applyData) {
+export default function useHTTP(applyData) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : 'GET',
-        headers: requestConfig.headers ? requestConfig.headers : {},
-        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-      });
+  const sendRequest = useCallback(
+    async requestConfig => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : 'GET',
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        });
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+
+        const data = await response.json();
+        applyData(data);
+      } catch (err) {
+        setError(err.message || 'Something went wrong!');
       }
-
-      const data = await response.json();
-      applyData(data);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+      setIsLoading(false);
+    },
+    [applyData]
+  );
 
   return { isLoading, error, sendRequest };
 }
