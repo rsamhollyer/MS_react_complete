e3752815ad34c463c14c9ffa9e67e5d72b2d32cf
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-11 11:39:49 -0500

    forms input

--- forms/src/components/BasicForm.jsx
+++ forms/src/components/BasicForm.jsx
@@ -1,25 +1,25 @@
-const BasicForm = (props) => {
-  return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
-        </div>
+import React from 'react';
+
+const BasicForm = () => (
+  <form>
+    <div className="control-group">
+      <div className="form-control">
+        <label htmlFor="name">First Name</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">Last Name</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
-      </div>
-    </form>
-  );
-};
+    </div>
+    <div className="form-control">
+      <label htmlFor="name">E-Mail Address</label>
+      <input type="text" id="name" />
+    </div>
+    <div className="form-actions">
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+);
 
 export default BasicForm;
--- forms/src/components/SimpleInput.jsx
+++ forms/src/components/SimpleInput.jsx
@@ -1,15 +1,36 @@
-const SimpleInput = (props) => {
+import React, { useRef, useState } from 'react';
+
+const SimpleInput = () => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+  const nameInputHander = e => {
+    setEnteredName(e.target.value);
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const nameValue = nameInputRef.current.value;
+    // nameInputRef.current.value = '' - This is not good practice because you should NOT manipulate the DOM directly
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputHander}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
 };
-
 export default SimpleInput;
