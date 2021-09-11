b25ac2883a8ee08946de90f97d2f34b99d93d7f4
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-11 11:51:37 -0500

    user validation updated

--- forms/src/components/SimpleInput.jsx
+++ forms/src/components/SimpleInput.jsx
@@ -2,6 +2,8 @@ import React, { useRef, useState } from 'react';
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState('');
+  const [isNameValid, setIsNameValid] = useState(true);
+
   // const nameInputRef = useRef();
   const nameInputHander = e => {
     setEnteredName(e.target.value);
@@ -9,19 +11,23 @@ const SimpleInput = () => {
 
   const submitHandler = e => {
     e.preventDefault();
+
     if (enteredName.trim() === '') {
+      setIsNameValid(false);
       return;
     }
+
     // const nameValue = nameInputRef.current.value;
 
     // nameInputRef.current.value = '' - This is not good practice because you should NOT manipulate the DOM directly
-
+    console.log(enteredName);
+    setIsNameValid(true);
     setEnteredName('');
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isNameValid && 'invalid'}`}>
         <label htmlFor="name">Your Name</label>
         <input
           // ref={nameInputRef}
@@ -30,6 +36,7 @@ const SimpleInput = () => {
           onChange={nameInputHander}
           value={enteredName}
         />
+        {!isNameValid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button type="submit">Submit</button>
