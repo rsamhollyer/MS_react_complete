08c55dde91c0f44627c87a3d80b1adf1823f034d
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-12 08:43:23 -0500

    update comp

--- forms/src/components/SimpleInput.jsx
+++ forms/src/components/SimpleInput.jsx
@@ -1,39 +1,29 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState('');
-  const [isNameValid, setIsNameValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  // const nameInputRef = useRef();
+  const isNameValid = enteredName.trim() !== '';
+
   const nameInputHander = e => {
     setEnteredName(e.target.value);
   };
 
-  const nameBlurHandler = e => {
+  const nameBlurHandler = () => {
     setEnteredNameTouched(true);
-
-    if (enteredName.trim() === '') {
-      setIsNameValid(false);
-    }
   };
 
   const submitHandler = e => {
     e.preventDefault();
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setIsNameValid(false);
+    if (!isNameValid) {
       return;
     }
-
-    // const nameValue = nameInputRef.current.value; - This works fine, but I'm using state for now
-
-    // nameInputRef.current.value = '' - This is not good practice because you should NOT manipulate the DOM directly
-
     console.log(enteredName);
-    setIsNameValid(true);
     setEnteredName('');
+    setEnteredNameTouched(false);
   };
 
   return (
@@ -45,7 +35,6 @@ const SimpleInput = () => {
       >
         <label htmlFor="name">Your Name</label>
         <input
-          // ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputHander}
