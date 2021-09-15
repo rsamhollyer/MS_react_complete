e1f8fb00c914a13091c136b4e3ebb4d0a6d8596b
Sam Hollyer <rsamhollyer@gmail.com>
2021-09-15 12:15:53 -0500

    initial push

---
+++ redux/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header = () => (
+  <header className={classes.header}>
+    <h1>Redux Auth</h1>
+    <nav>
+      <ul>
+        <li>
+          <a href="/">My Products</a>
+        </li>
+        <li>
+          <a href="/">My Sales</a>
+        </li>
+        <li>
+          <button type="button">Logout</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
