5b6e6fd6e1145e1b087964f223c8679663bd4e76
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-06 14:14:55 -0500

    set up routes

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
+
+/*
+  Note for Snowpack : Make sure to alter the config file at the `routes` section. Need to uncomment the line.
+*/
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Switch>
+        <Route path="/welcome" component={Welcome} />
+        <Route path="/products" component={Products} />
+      </Switch>
     </div>
   );
 }
