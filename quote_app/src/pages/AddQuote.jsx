a9423e7da3b4711ade3752c60073be72227267f8
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-07 09:22:25 -0500

    add history hook to quoteform

--- quote_app/src/pages/AddQuote.jsx
+++ quote_app/src/pages/AddQuote.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 export default function AddQuote() {
+  const history = useHistory();
   const addQuoteHandler = quoteData => {
     console.log(quoteData);
+    history.push('/quotes');
   };
   return (
     <div>
