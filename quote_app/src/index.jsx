5195d137198be9b2d431a1c40dd4017281511b9f
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-07 06:54:34 -0500

    add 3 pages

--- quote_app/src/index.jsx
+++ quote_app/src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
---
+++ quote_app/src/pages/AddQuote.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AddQuote() {
+  return <div />;
+}
