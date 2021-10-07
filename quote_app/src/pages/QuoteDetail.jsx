51b87d3bb708d3c2a3c4b5c01da70e628e82864b
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-07 07:09:18 -0500

    add useparams in detail page

--- quote_app/src/pages/QuoteDetail.jsx
+++ quote_app/src/pages/QuoteDetail.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function QuoteDetail() {
+  const params = useParams();
   return (
     <div>
       <h2>Quote Detail</h2>
+      <p>{params.quoteId}</p>
     </div>
   );
 }
