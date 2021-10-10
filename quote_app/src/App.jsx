92f944e150a0bf6237c62eaa0351546585814d05
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-10 09:08:35 -0500

    code splitting app page imports

--- quote_app/src/App.jsx
+++ quote_app/src/App.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import AddQuote from './pages/AddQuote';
-import AllQuotes from './pages/AllQuotes';
-import NotFound from './pages/NotFound';
-import QuoteDetail from './pages/QuoteDetail';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+// import AddQuote from './pages/AddQuote';
+// import AllQuotes from './pages/AllQuotes';
+// import NotFound from './pages/NotFound';
+// import QuoteDetail from './pages/QuoteDetail';
+
+/**
+ * Code Splitting
+ */
+
+const AddQuote = React.lazy(() => import('./pages/AddQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/quotes" />} />
-        <Route exact path="/quotes" component={AllQuotes} />
-        <Route path="/quotes/:quoteId" component={QuoteDetail} />
-        <Route path="/new-quote" component={AddQuote} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/quotes" />} />
+          <Route exact path="/quotes" component={AllQuotes} />
+          <Route path="/quotes/:quoteId" component={QuoteDetail} />
+          <Route path="/new-quote" component={AddQuote} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
