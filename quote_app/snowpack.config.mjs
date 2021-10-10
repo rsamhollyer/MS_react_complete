684a7c65beb88910c9f48caef4096dc8e1e096a3
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-10 09:23:56 -0500

    install webpach, configure snowpack build

--- quote_app/snowpack.config.mjs
+++ quote_app/snowpack.config.mjs
@@ -4,14 +4,20 @@ export default {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-webpack',
+  ],
   routes: [
     /* Enable an SPA Fallback in development: */
     { match: 'routes', src: '.*', dest: '/index.html' },
   ],
   optimize: {
     /* Example: Bundle your final build: */
-    // "bundle": true,
+    bundle: true,
+    minify: true,
+    treeshake: true,
   },
   packageOptions: {
     /* ... */
