399060b52655350e3809ff05eefcc3dd2b30413f
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-14 12:43:39 -0500

    set logic to handle auto timeout and clear local storage

--- auth/src/store/AuthContext.jsx
+++ auth/src/store/AuthContext.jsx
@@ -7,6 +7,14 @@ const initialData = {
   logout: () => {},
 };
 
+const calculateRemainingTime = expirationTime => {
+  const currentTime = new Date().getTime();
+  const adjustedTime = new Date(expirationTime).getTime();
+  const remainingTime = adjustedTime - currentTime;
+
+  return remainingTime;
+};
+
 export const AuthContext = createContext(initialData);
 
 export default function AuthContextProvider({ children }) {
@@ -15,13 +23,17 @@ export default function AuthContextProvider({ children }) {
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = someToken => {
-    setToken(someToken);
-    localStorage.setItem('token', someToken);
-  };
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token');
+  };
+
+  const loginHandler = (someToken, expireAt) => {
+    setToken(someToken);
+    localStorage.setItem('token', someToken);
+    const expirationTime = calculateRemainingTime(expireAt);
+
+    setTimeout(logoutHandler, expirationTime);
   };
 
   const contextValue = {
