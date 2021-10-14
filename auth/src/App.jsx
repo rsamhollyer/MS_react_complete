4690ceb8982ae706ef719d21083e34f5c91f55f3
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-14 09:25:56 -0500

    route blocking added

--- auth/src/App.jsx
+++ auth/src/App.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { AuthContext } from './store/AuthContext';
 
 function App() {
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/auth">
-          <AuthPage />
-        </Route>
-        <Route path="/profile">
-          <UserProfile />
-        </Route>
+        <Route path="/" exact component={HomePage} />
+        {!isLoggedIn && <Route path="/auth" component={AuthPage} />}
+        {isLoggedIn && <Route path="/profile" component={UserProfile} />}
+        <Route path="*" render={() => <Redirect to="/" />} />
       </Switch>
     </Layout>
   );
