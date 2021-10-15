2e93fc46af1d183ea48e0635ad194db01e9355ac
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-15 08:47:08 -0500

    set up dummy data

--- meetup_next/pages/index.jsx
+++ meetup_next/pages/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+import { DUMMY_DATA } from '../utils/meetups';
 
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return <MeetupList meetups={DUMMY_DATA} />;
 }
---
+++ meetup_next/utils/meetups.js
@@ -0,0 +1,26 @@
+export const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/4/40/Panor%C3%A1mica_Oto%C3%B1o_Alc%C3%A1zar_de_Segovia.jpg',
+    address: '123 First Street',
+    description: 'First Meetup',
+  },
+  {
+    id: 'm2',
+    title: 'Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/4/40/Panor%C3%A1mica_Oto%C3%B1o_Alc%C3%A1zar_de_Segovia.jpg',
+    address: '123 First Street',
+    description: 'First Meetup',
+  },
+  {
+    id: 'm3',
+    title: 'Thirds Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/4/40/Panor%C3%A1mica_Oto%C3%B1o_Alc%C3%A1zar_de_Segovia.jpg',
+    address: '123 First Street',
+    description: 'First Meetup',
+  },
+];
