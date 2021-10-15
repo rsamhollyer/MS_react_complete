1419cbf6275b8eb0c6af0e26ee1e84c81b1caa9f
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-15 08:00:24 -0500

    fixes

--- next_intro/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-// our-domain.com/
-import React from 'react';
-
-export default function Home() {
-  return <h1>Home Page</h1>;
-}
--- next_intro/pages/news.jsx
+++
@@ -1,7 +0,0 @@
-// our-domain.com/news
-
-import React from 'react';
-
-export default function News() {
-  return <h1>News Page</h1>;
-}
