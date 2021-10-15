917b8ad8c30f5c2db0de205dd16d8f4add7f210e
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-15 08:15:44 -0500

    start dynamic routing

---
+++ next_intro/pages/news/[newsId].jsx
@@ -0,0 +1,7 @@
+// our-domain.com/news/:newsId - dynamic routing is written like [].jsx with an identifier of some-sort in the brackets
+
+import React from 'react';
+
+export default function DetailPage() {
+  return <h1>A nested page</h1>;
+}
--- next_intro/pages/news/something-else.jsx
+++
@@ -1,7 +0,0 @@
-// our-domain.com/news/something-else
-
-import React from 'react';
-
-export default function somethingElse() {
-  return <h1>A nested page</h1>;
-}
