447843ba31441b671d0e705aba3a47ff03da25da
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-15 08:19:54 -0500

    get dynamic id with hook

--- next_intro/pages/news/[newsId].jsx
+++ next_intro/pages/news/[newsId].jsx
@@ -1,7 +1,13 @@
 // our-domain.com/news/:newsId - dynamic routing is written like [].jsx with an identifier of some-sort in the brackets
 
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function DetailPage() {
+  const router = useRouter();
+  const { newsId } = router.query;
+  /**
+   * If we had server and database, we can use this id to fetch what we need to display
+   */
   return <h1>A nested page</h1>;
 }
