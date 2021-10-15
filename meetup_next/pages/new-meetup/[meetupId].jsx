e0180ec1fd7dd867874e93d274fd9b553bf84d6f
Sam Hollyer <rsamhollyer@gmail.com>
2021-10-15 08:36:54 -0500

    initial push

---
+++ meetup_next/pages/new-meetup/[meetupId].jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MeetupDetailsPage() {
+  return <h1>Details of Meetup</h1>;
+}
---
+++ meetup_next/pages/new-meetup/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NewMeetupPage() {
+  return <h1>New Meetup</h1>;
+}
