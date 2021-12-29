d31f7513aae693ee2c2bae948f7b8f39d5069368
Sam Hollyer <rsamhollyer@gmail.com>
2021-12-29 14:44:59 -0600

    connect mongo database to api route

--- meetup_next/pages/api/new-meetup.js
+++ meetup_next/pages/api/new-meetup.js
@@ -3,8 +3,26 @@
  * POST ONLY
  */
 
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { title, image, address, description } = req.body;
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.ggmv6.mongodb.net/meetups?retryWrites=true&w=majority`
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne({
+      title,
+      image,
+      address,
+      description,
+    });
+    client.close();
+    res.status(201).json({ message: 'Meetup Inserted' });
   }
 }
