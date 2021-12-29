e41a7d8647f0fcde69a6df48107f7a5ca0f5f28c
Sam Hollyer <rsamhollyer@gmail.com>
2021-12-29 13:45:20 -0600

    add ssr function as example

--- meetup_next/pages/index.jsx
+++ meetup_next/pages/index.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import MeetupList from '../components/meetups/MeetupList';
-import { DUMMY_DATA } from '../utils/meetups';
 
 export default function HomePage({ meetups }) {
   return <MeetupList meetups={meetups} />;
 }
 
-// Moves data fetching to build process side of thigns
+// Moves data fetching to build process side of things - caching
+
 export async function getStaticProps() {
   return {
     props: {
       meetups: DUMMY_DATA,
     },
-    revalidate: 10, // Seconds Nextjs will wait before regenerating the page for incoming request - data is never older than 10seconds. This is use dependent
+    revalidate: 1, // Seconds Nextjs will wait before regenerating the page for incoming requests. This is use dependent.
   };
 }
+
+Runs on the server AFTER deployment
+// Only need if you need request object OR have data that changes constantly - not good for this particular project
+
+// export async function getServerSideProps(context) {
+//   const { req, res } = context; // Headers and other information you might want
+
+//   return {
+//     props: {
+//       meetups: DUMMY_DATA,
+//     },
+//   };
+// }
