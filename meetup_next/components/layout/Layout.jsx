cd524a9072f538223615f5ee958fc68819030d7f
Sam Hollyer <rsamhollyer@gmail.com>
2021-12-29 07:20:22 -0600

    update page layout for all pages via _app

--- meetup_next/components/layout/Layout.jsx
+++ meetup_next/components/layout/Layout.jsx
@@ -1,11 +1,11 @@
 import classes from './Layout.module.css';
 import MainNavigation from './MainNavigation';
 
-function Layout(props) {
+function Layout({ children }) {
   return (
     <div>
       <MainNavigation />
-      <main className={classes.main}>{props.children}</main>
+      <main className={classes.main}>{children}</main>
     </div>
   );
 }
