e18d77d43de5f7bf3e033e85f4679e89e23ff859
Sam Hollyer <rsamhollyer@gmail.com>
2021-12-29 08:21:57 -0600

    add useeffect and usestate to home page

--- meetup_next/pages/index.jsx
+++ meetup_next/pages/index.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 import { DUMMY_DATA } from '../utils/meetups';
 
 export default function HomePage() {
-  return <MeetupList meetups={DUMMY_DATA} />;
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  useEffect(() => {
+    setLoadedMeetups(DUMMY_DATA);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
 }
