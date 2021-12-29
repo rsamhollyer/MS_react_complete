a9b35ed6c54a3f0cd58bf7c28cd49461de56089d
Sam Hollyer <rsamhollyer@gmail.com>
2021-12-29 13:23:23 -0600

    add site revalidating

--- meetup_next/pages/index.jsx
+++ meetup_next/pages/index.jsx
@@ -6,11 +6,12 @@ export default function HomePage({ meetups }) {
   return <MeetupList meetups={meetups} />;
 }
 
+// Moves data fetching to build process side of thigns
 export async function getStaticProps() {
-  // Moves data fetching to build process side of thigns
   return {
     props: {
       meetups: DUMMY_DATA,
     },
+    revalidate: 10, // Seconds Nextjs will wait before regenerating the page for incoming request - data is never older than 10seconds. This is use dependent
   };
 }
