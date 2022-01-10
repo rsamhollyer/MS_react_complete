41e556cdd6694e6cb2885f264f3668a153aca311
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-10 16:19:47 -0600

    add configurestore comp/dispatcher

---
+++ hooks-no-redux/src/hooks/configureStore.jsx
@@ -0,0 +1,45 @@
+import { initStore } from './useStore';
+
+export function configureStore() {
+  const actions = {
+    TOGGLE_FAV: (curState, productId) => {
+      const prodIndex = curState.product.findIndex(p => p.id === productId);
+      const newFavStatus = !curState.product[prodIndex].isFavorite;
+      const updatedProducts = [...curState.product];
+      updatedProducts[prodIndex] = {
+        ...curState.product[prodIndex],
+        isFavorite: newFavStatus,
+      };
+
+      return { products: updatedProducts };
+    },
+  };
+  initStore(actions, {
+    products: [
+      {
+        id: 'p1',
+        title: 'Red Scarf',
+        description: 'A pretty red scarf.',
+        isFavorite: false,
+      },
+      {
+        id: 'p2',
+        title: 'Blue T-Shirt',
+        description: 'A pretty blue t-shirt.',
+        isFavorite: false,
+      },
+      {
+        id: 'p3',
+        title: 'Green Trousers',
+        description: 'A pair of lightly green trousers.',
+        isFavorite: false,
+      },
+      {
+        id: 'p4',
+        title: 'Orange Hat',
+        description: 'Street style! An orange hat.',
+        isFavorite: false,
+      },
+    ],
+  });
+}
