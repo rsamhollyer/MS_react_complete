38e94bd8dec72fc25490f314d80332252327f3ea
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-10 07:41:09 -0600

    add animation

---
+++ animation-1/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
+import Modal from './components/Modal/Modal';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false,
+    };
+  }
+
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        <Backdrop />
+        <button type="button" className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
