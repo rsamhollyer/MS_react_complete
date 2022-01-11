4c98a5491b73ac21e3d1f4b0cf36cfb160037360
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-11 07:24:34 -0600

    add grouping test

--- 01-starting-project/src/components/Greeting.test.js
+++ 01-starting-project/src/components/Greeting.test.js
@@ -1,15 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders hello as a text', () => {
-  /* Arrange */
-  render(<Greeting />);
+describe('Greeting component', () => {
+  test('renders Hello World as a text', () => {
+    /* Arrange */
+    render(<Greeting />);
 
-  /* Act */
-  // ...nothing
+    /* Act */
+    // ...nothing
 
-  /* Asset */
-  const helloWorldElement = screen.getByText('Hello World', { exact: false });
-  // Check whethere helloWorldElement exists
-  expect(helloWorldElement).toBeInTheDocument();
+    /* Asset */
+    const helloWorldElement = screen.getByText('Hello World', { exact: false });
+    // Check whethere helloWorldElement exists
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
