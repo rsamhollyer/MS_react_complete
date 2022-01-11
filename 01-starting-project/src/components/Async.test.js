69dc63456aa3229b46131488eef18913907d8f11
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-11 08:14:01 -0600

    add mocked fetch test functionality

--- 01-starting-project/src/components/Async.test.js
+++ 01-starting-project/src/components/Async.test.js
@@ -3,6 +3,14 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
+    /* This will simulate the built in window fetch funciton using a mock fetch built into jest. We do this to avoid making network calls to 3rd party data or our own data base, because we're testing that our expected results are matched, not if the connection is necessarily made, at least in this example
+     */
+
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    });
+
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole(
