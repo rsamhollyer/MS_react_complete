9d671b49d2bfa8c445ca3fcb544ef22aaeec69ad
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-11 07:45:59 -0600

    add userevent to testing for button push

--- 01-starting-project/src/components/Greeting.jsx
+++ 01-starting-project/src/components/Greeting.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
-      <h2>Hello World</h2>
-      <p>Nice to meet you!</p>
+      <h2>Hello World!</h2>
+      {!changedText && <p>Nice to meet you!</p>}
+      {changedText && <p>Changed!</p>}
+      <button type="button" onClick={changeTextHandler}>
+        Change Text!
+      </button>
     </div>
   );
 }
--- 01-starting-project/src/components/Greeting.test.js
+++ 01-starting-project/src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
@@ -9,9 +10,31 @@ describe('Greeting component', () => {
     /* Act */
     // ...nothing
 
-    /* Asset */
+    /* Assert */
     const helloWorldElement = screen.getByText('Hello World', { exact: false });
     // Check whethere helloWorldElement exists
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders Nice to meet you if the button was NOT clicked', () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText('Nice to meet you', {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders Changed if the button was clicked', () => {
+    render(<Greeting />);
+
+    /* Act */
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    /* Assert */
+    const outputElement = screen.getByText('Changed', {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
   });
 });
