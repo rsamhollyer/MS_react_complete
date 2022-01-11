f966c8147ff82987cb52caf52fc3274c02aa6693
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-11 08:05:52 -0600

    add async component and async test

---
+++ 01-starting-project/src/components/Async.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Async from './Async';
+
+describe('Async component', () => {
+  test('renders posts if request succeeds', async () => {
+    render(<Async />);
+
+    const listItemElements = await screen.findAllByRole(
+      'listitem',
+      { exact: true },
+      { timeout: 5000 }
+    );
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
