7cc54f41aa3c29af468dd65dba8a67accf624567
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-29 09:52:10 -0600

    add post request to send ingred to firebase

--- hooks-summary/src/components/Ingredients/Ingredients.jsx
+++ hooks-summary/src/components/Ingredients/Ingredients.jsx
@@ -3,15 +3,26 @@ import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
+export const URLString =
+  'https://auth-312de-default-rtdb.firebaseio.com/ingredients.json';
+
+export async function fetchPostIngreds(url, bodyString) {
+  const res = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(bodyString),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const response = await res.json();
+  return response;
+}
+
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredientHandler = newIngredient => {
-    const newId = new Date().getTime();
-    setIngredients(prevState => [
-      ...prevState,
-      { id: newId, ...newIngredient },
-    ]);
+  const addIngredientHandler = async newIngredient => {
+    const getFirebaseId = await fetchPostIngreds(URLString, newIngredient);
+    const id = getFirebaseId.name;
+    setIngredients(prevState => [...prevState, { id, ...newIngredient }]);
   };
 
   const removeIngredientHandler = igID => {
